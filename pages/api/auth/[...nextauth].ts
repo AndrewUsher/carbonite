62bf0eac31f0bc699111d15ec5167838c9280465
Andrew Usher <andrewusher00@gmail.com>
2021-02-08 19:28:32 -0600

    feat: add auth support

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,8 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+// @ts-expect-error types for next-auth are currently incorrect
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
@@ -10,6 +10,11 @@ export default NextAuth({
     })
     // ...add more providers here
   ],
+  events: {
+    async createUser (message) {
+      console.log(message)
+    }
+  },
 
   // A database is optional, but required to persist accounts in a database
   database: process.env.DATABASE_URL
