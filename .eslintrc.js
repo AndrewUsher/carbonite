9fe4a5a3683e9bdf6b7df25d605b3ede7d7e6a08
Andrew Usher <andrewusher00@gmail.com>
2021-02-14 14:44:33 -0600

    refactor: ignore non null assertions for environment variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@drewster/eslint-config/react', '@drewster/eslint-config/ts'],
   rules: {
-    'react/react-in-jsx-scope': 0
+    'react/react-in-jsx-scope': 0,
+    '@typescript-eslint/no-non-null-assertion': 0
   }
 }
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,17 +5,9 @@ import Providers from 'next-auth/providers'
 export default NextAuth({
   providers: [
     Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!
     })
-    // ...add more providers here
   ],
-  events: {
-    async createUser (message) {
-      console.log(message)
-    }
-  },
-
-  // A database is optional, but required to persist accounts in a database
   database: process.env.DATABASE_URL
 })
