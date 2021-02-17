407feee5115379f25602a6d348c7a39799cf100b
Andrew Usher <andrewusher00@gmail.com>
2021-02-16 22:32:55 -0600

    feat: add inital tests for homepage

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   extends: ['@drewster/eslint-config/react', '@drewster/eslint-config/ts'],
+  globals: {
+    mocha: true
+  }
   rules: {
     'react/react-in-jsx-scope': 0,
     '@typescript-eslint/no-non-null-assertion': 0
