8780e6aec87b07260fa462444d20219736a45c70
Andrew Usher <andrew.usher@autozone.com>
2021-04-22 12:32:58 -0500

    feat: add browse button to nav

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,10 +3,15 @@ import { Flex, Heading, Button, Menu, MenuButton, MenuList, MenuItem } from '@ch
 import { signIn, signOut, useSession } from 'next-auth/client'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import { ChevronDownIcon } from '@chakra-ui/icons'
 
 export const Navigation: FC = () => {
   const [session, sessionInfoLoading] = useSession()
   const router = useRouter()
+
+  const navigateTo = (path: string) => () => {
+    router.push(path)
+  }
 
   const navigateToFavorites = () => {
     router.push('/favorites')
@@ -28,24 +33,46 @@ export const Navigation: FC = () => {
           Carbonite
         </Link>
       </Heading>
-      {session ? (
-        <Menu colorScheme="purple">
+      <Flex>
+        <Menu colorScheme="blue">
           <MenuButton
             as={Button}
-            leftIcon={session.user.image ? <img width={30} src={session.user.image} alt=""/> : null}
+            rightIcon={<ChevronDownIcon />}
             variant="solid"
-            colorScheme="purple"
+            colorScheme="blue"
+            mr={4}
           >
-            {session.user.name}
+          Browse
           </MenuButton>
           <MenuList color="black">
-            <MenuItem onClick={navigateToFavorites}>Favorites</MenuItem>
-            <MenuItem onClick={handleSignOut}>Sign Out</MenuItem>
+            <MenuItem onClick={navigateTo('/films')}>Films</MenuItem>
+            <MenuItem onClick={navigateTo('/people')}>People</MenuItem>
+            <MenuItem onClick={navigateTo('/planets')}>Planets</MenuItem>
+            <MenuItem onClick={navigateTo('/species')}>Species</MenuItem>
+            <MenuItem onClick={navigateTo('/starships')}>Starships</MenuItem>
+            <MenuItem onClick={navigateTo('/vehicles')}>Vehicles</MenuItem>
           </MenuList>
         </Menu>
-      ) : (
-        <Button variant="solid" colorScheme="purple" isLoading={sessionInfoLoading} onClick={handleSignIn}>Sign In</Button>
-      )}
+
+        {session ? (
+          <Menu colorScheme="purple">
+            <MenuButton
+              as={Button}
+              leftIcon={session.user.image ? <img width={30} src={session.user.image} alt=""/> : null}
+              variant="solid"
+              colorScheme="purple"
+            >
+              {session.user.name}
+            </MenuButton>
+            <MenuList color="black">
+              <MenuItem onClick={navigateToFavorites}>Favorites</MenuItem>
+              <MenuItem onClick={handleSignOut}>Sign Out</MenuItem>
+            </MenuList>
+          </Menu>
+        ) : (
+          <Button variant="solid" colorScheme="purple" isLoading={sessionInfoLoading} onClick={handleSignIn}>Sign In</Button>
+        )}
+      </Flex>
     </Flex>
   )
 }
