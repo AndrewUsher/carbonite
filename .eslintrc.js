3e7dd8c79441a99c9801b1c1b3e43d5f28bdfd58
Andrew Usher <andrewusher00@gmail.com>
2026-05-23 21:45:49 -0500

    upgrade Next.js 16.2.6, React 19.2.6, Chakra UI v2, and related deps
    Major version bumps:
    - next: 12.1.6 -> 16.2.6 (Pages Router preserved)
    - react/react-dom: 17.0.2 -> 19.2.6
    - @chakra-ui/react: 1.6.5 -> 2.10.9
    - framer-motion: 4.1.17 -> 12.40.0
    - @emotion/react: 11.4.0 -> 11.14.0
    - @emotion/styled: 11.3.0 -> 11.14.1
    - typescript: 4.2.3 -> 5.9.3
    - eslint: 7.31.0 -> 8.57.1
    - eslint-config-next: 11.1.0 -> 15.5.18
    
    Removed: @chakra-ui/system (re-exported by v2)
    
    Config updates:
    - next.config.js: removed swcMinify (default in v16)
    - tsconfig: target es2017, moduleResolution bundler, skipLibCheck true
    - .eslintrc: simplified to next/core-web-vitals
    - CI: lint workflow uses Node LTS, checkout@v5, setup-node@v6

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,21 @@
 module.exports = {
-  extends: ['@drewster/eslint-config/react', '@drewster/eslint-config/ts'],
-  globals: {
-    mocha: true
-  },
+  root: true,
+  extends: [
+    'next/core-web-vitals',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
     camelcase: 0,
     'react/react-in-jsx-scope': 0,
-    '@typescript-eslint/no-non-null-assertion': 0
-  }
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    'no-use-before-define': 0,
+    '@typescript-eslint/no-use-before-define': ['error'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 }
--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  swcMinify: true,
   typescript: {
     ignoreBuildErrors: true
   }
